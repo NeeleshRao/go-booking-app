f7122947fd8d864dcce4e0dd853dd32900ae0d62
Neelesh Thonse Rao <neeleshrao1@gmail.com>
2024-09-27 21:48:39 +0530

    User Input and pointers

--- main.go
+++ main.go
@@ -3,26 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	// var confName = "Python Convention"
-	// const confTickets = 50
-	// var remainingTickets = 50
-	// another operator for type inferring := but this cannot be done outside function
-	// other ways of declaration with types rather than inferring
+	
 	var confName string = "Go Conference"
 	const confTickets int = 50
 	var remainingTickets uint32 = 50
 
-	// To view types of the variables
-	fmt.Printf("The types of variables confName = %T, confTickets = %T and remainingTickets = %T\n", confName, confTickets, remainingTickets)
-
+	// to understand pointers in go, it is very simple. 
+	// Every variable basically references where a value is stored.
+	// A pointer holds the address of where the value is/will be stored in memory
+	fmt.Println(confName, &confName)
+	
 	fmt.Printf("Welcome to the %v conference!\n", confName)
 	fmt.Printf("There are a total of %v tickets with only %v remaining...\n", confTickets, remainingTickets)
 
-	var userName string
-	var numTickets uint
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint32
 
-	userName = "neelesh ig"
-	numTickets = 2
+	fmt.Println("Enter your first name : ")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("User %v wants %v tickets.\n", userName, numTickets)
+	fmt.Println("Enter your last name : ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email id : ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets you want : ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets\n", firstName, lastName, userTickets)
+	fmt.Printf("A confirmation mail will be sent to your email - %v\n", email)
 }
