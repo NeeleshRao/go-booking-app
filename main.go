ab09455156809575d01f13c69d64e1e408b73ae7
Neelesh Thonse Rao <neeleshrao1@gmail.com>
2024-09-28 07:13:47 +0530

    arrays and basic logic

--- main.go
+++ main.go
@@ -7,12 +7,8 @@ func main() {
 	var confName string = "Go Conference"
 	const confTickets int = 50
 	var remainingTickets uint32 = 50
+	var booking [50]string
 
-	// to understand pointers in go, it is very simple. 
-	// Every variable basically references where a value is stored.
-	// A pointer holds the address of where the value is/will be stored in memory
-	fmt.Println(confName, &confName)
-	
 	fmt.Printf("Welcome to the %v conference!\n", confName)
 	fmt.Printf("There are a total of %v tickets with only %v remaining...\n", confTickets, remainingTickets)
 
@@ -33,6 +29,15 @@ func main() {
 	fmt.Println("Enter number of tickets you want : ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets -= userTickets
+	booking[0] = firstName + " " + lastName
+
+	fmt.Printf("The booking array is %v\n", booking)
+	fmt.Printf("The first element of the array is %v\n", booking[0])
+	fmt.Printf("The type of the array is %T\n", booking)
+	fmt.Printf("The length of the array is %v\n", len(booking))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets\n", firstName, lastName, userTickets)
 	fmt.Printf("A confirmation mail will be sent to your email - %v\n", email)
+	fmt.Printf("For the event %v, only %v seats remain!\n", confName, remainingTickets)
 }
