a0487693176737009e2ec19e40a3e42ca35a7302
Neelesh Thonse Rao <neeleshrao1@gmail.com>
2024-09-28 11:10:28 +0530

    loops and basic conditionals added

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
@@ -12,33 +15,44 @@ func main() {
 	fmt.Printf("Welcome to the %v conference!\n", confName)
 	fmt.Printf("There are a total of %v tickets with only %v remaining...\n", confTickets, remainingTickets)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint32
+	for remainingTickets > 0 {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint32
 
-	fmt.Println("Enter your first name : ")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your first name : ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name : ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name : ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email id : ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email id : ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter number of tickets you want : ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter number of tickets you want : ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets -= userTickets
+		if userTickets > remainingTickets {
+			fmt.Printf("There are only %v tickets remaining, please try again!!\n", remainingTickets)
+			continue
+		}
 
-	booking = append(booking, firstName+" "+lastName)
+		remainingTickets -= userTickets
 
-	fmt.Printf("The booking slice is %v\n", booking)
-	fmt.Printf("The first element of the slice is %v\n", booking[0])
-	fmt.Printf("The type of the slice is %T\n", booking)
-	fmt.Printf("The length of the slice is %v\n", len(booking))
+		fmt.Printf("Thank you %v %v for booking %v tickets\n", firstName, lastName, userTickets)
+		fmt.Printf("A confirmation mail will be sent to your email - %v\n", email)
+		fmt.Printf("For the event %v, only %v seats remain!\n", confName, remainingTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets\n", firstName, lastName, userTickets)
-	fmt.Printf("A confirmation mail will be sent to your email - %v\n", email)
-	fmt.Printf("For the event %v, only %v seats remain!\n", confName, remainingTickets)
+		booking = append(booking, firstName+" "+lastName)
+
+		var onlyFirstNames []string
+
+		for _, book := range booking {
+			onlyFirstNames = append(onlyFirstNames, strings.Split(book, " ")[0])
+		}
+
+		fmt.Printf("These are all the bookings till date : %v\n", onlyFirstNames)
+	}
+
 }
