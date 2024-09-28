861e5ec464eaf99165a63e711f5119e19bced724
Neelesh Thonse Rao <neeleshrao1@gmail.com>
2024-09-28 12:11:31 +0530

    user input validation

--- main.go
+++ main.go
@@ -33,26 +33,40 @@ func main() {
 		fmt.Println("Enter number of tickets you want : ")
 		fmt.Scan(&userTickets)
 
-		if userTickets > remainingTickets {
-			fmt.Printf("There are only %v tickets remaining, please try again!!\n", remainingTickets)
-			continue
+		// validating user input
+		var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
+		var isValidEmail = strings.Contains(email, "@")
+		var isValidTickets = userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTickets {
+			remainingTickets -= userTickets
+
+			fmt.Printf("Thank you %v %v for booking %v tickets\n", firstName, lastName, userTickets)
+			fmt.Printf("A confirmation mail will be sent to your email - %v\n", email)
+			fmt.Printf("For the event %v, only %v seats remain!\n", confName, remainingTickets)
+
+			booking = append(booking, firstName+" "+lastName)
+
+			var onlyFirstNames []string
+
+			for _, book := range booking {
+				onlyFirstNames = append(onlyFirstNames, strings.Split(book, " ")[0])
+			}
+
+			fmt.Printf("These are all the bookings till date : %v\n", onlyFirstNames)
+
+		} else {
+			if !isValidName {
+				fmt.Println("You have entered a very short name (min 2 characters)!")
+			}
+			if !isValidEmail {
+				fmt.Println("You did not enter a valid email!")
+			}
+			if !isValidTickets {
+				fmt.Printf("There are only %v tickets remaining, please try again!\n", remainingTickets)
+			}
 		}
 
-		remainingTickets -= userTickets
-
-		fmt.Printf("Thank you %v %v for booking %v tickets\n", firstName, lastName, userTickets)
-		fmt.Printf("A confirmation mail will be sent to your email - %v\n", email)
-		fmt.Printf("For the event %v, only %v seats remain!\n", confName, remainingTickets)
-
-		booking = append(booking, firstName+" "+lastName)
-
-		var onlyFirstNames []string
-
-		for _, book := range booking {
-			onlyFirstNames = append(onlyFirstNames, strings.Split(book, " ")[0])
-		}
-
-		fmt.Printf("These are all the bookings till date : %v\n", onlyFirstNames)
 	}
 
 }
